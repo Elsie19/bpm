d1cdffda53be68a5af82728ed70372c2c76b6ac6
The TwilightBlood <hwengerstickel@protonmail.com>
2021-11-30 13:13:13 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,14 @@ sudo make install
 cd ..
 rm -rf semver-tool
 
+git clone --depth=1 https://github.com/ajdiaz/bashc
+cd bashc
+make -j$(nproc)
+chmod +x releases/bashc
+sudo install -dm755 releases/bashc /usr/bin/bashc
+cd ..
+rm -rf bashc
+
 git clone --depth=1 https://github.com/Henryws/bpm
 cd bpm
 sudo install -dm755 bpm /bin/bpm
